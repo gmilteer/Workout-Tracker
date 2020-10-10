6a9aabf868d24d1c7b3aeacdfdd415225e6b6bb2
Georgia Milteer <gmilteer@gmail.com>
2020-10-10 18:11:18 -0400

    deploying

--- server.js
+++ server.js
@@ -11,9 +11,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+var MONGODB_URI =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://gmilteer:Myrtleamy12@cluster0.gpnge.mongodb.net/<dbname>?retryWrites=true&w=majority";
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false,
 });
 
